2a7b3ce39c0a30d3c5d74f5a77cb7f798feca361
jackson05 <jackson5g.bi@gmail.com>
2021-12-14 23:36:23 +0200

    implementation of mail service

--- src/main/java/com/jackson/java/mail/javamail/service/MailService.java
+++ src/main/java/com/jackson/java/mail/javamail/service/MailService.java
@@ -1,5 +1,29 @@
 package com.jackson.java.mail.javamail.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MailService {
 
+	@Autowired
+	private JavaMailSender mailSender;
+	
+	public String sendMail(String mailTo,String subject,String body ) {
+		try {
+		SimpleMailMessage mailMessage=new SimpleMailMessage();
+		mailMessage.setFrom("exemple.from@gmail.com");
+		mailMessage.setTo(mailTo);
+		mailMessage.setSubject(subject);
+		mailMessage.setText(body);
+		mailSender.send(mailMessage);
+		System.out.println("mail sent");
+		return "message sent";
+		}catch (Exception e) {
+			System.out.println("mail not sent");
+			return e.getMessage();
+		}
+	}
 }
